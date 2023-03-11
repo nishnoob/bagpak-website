6f1d8d30d6c02e62c98a4abf9e87610ad521ef3d
Agnish Mukherjee <agnish15m@gmail.com>
2023-03-11 17:59:34 +0530

    Added gtm

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,13 +5,26 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <>
       <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
+      {/* <!-- Google Tag Manager --> */}
+      <script dangerouslySetInnerHTML={{__html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5J5N3R7');
+      `}}></script>
+      {/* <!-- End Google Tag Manager --> */}
+      
+      {/* <!-- Google Tag Manager (noscript) --> */}
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5J5N3R7"
+      height="0" width="0" style={{display:'none', visibility:'hidden'}}></iframe></noscript>
+      {/* <!-- End Google Tag Manager (noscript) --> */}
 
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
       <link rel="manifest" href="/site.webmanifest"></link>
       <Head>
-
         {/* <!-- Primary Meta Tags --> */}
         <title>Bagpaks | we make things MOVE</title>
         <meta name="title" content="Bagpaks | we make things MOVE" />
